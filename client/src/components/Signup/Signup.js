13a534acd3c6b3b0da840961ccbcd554517287f0
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-07-24 12:35:37 +0530

    Code formating

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -4,62 +4,65 @@ import './Signup.css'
 function Signup (props) {
   return (
     <div className="Container">
-    <div className="column">
-      <div className="header">        
-        <img src={ process.env.PUBLIC_URL + "images/jioflix.png" } title="logo" alt="logo" />
-        <h3>Sign Up</h3>
-      </div>
-      <form action="" method="POST">
+        <div className="column">
 
-        <div className="txt_field">
-          <input type="text" required />
-          <span></span>
-          <label>First name</label>
+          <div className="header">        
+            <img src={ process.env.PUBLIC_URL + "images/jioflix.png" } title="logo" alt="logo" />
+            <h3>Sign Up</h3>
+          </div>
+
+          <form>
+            <div className="txt_field">
+              <input type="text" required />
+              <span></span>
+              <label>First name</label>
+            </div>
+
+            <div className="txt_field">
+              <input type="text" required />
+              <span></span>
+              <label>Last name</label>
+            </div>
+
+            <div className="txt_field">
+              <input type="text" required />
+              <span></span>
+              <label>Username</label>
+            </div>
+
+            <div className="txt_field">
+              <input type="email" required />
+              <span></span>
+              <label>Email</label>
+            </div>
+
+            <div className="txt_field">
+              <input type="email" required />
+              <span></span>
+              <label>Confirm email</label>
+            </div>
+
+            <div className="txt_field">
+              <input type="password" required />
+              <span></span>
+              <label>Password</label>
+            </div>
+
+            <div className="txt_field">
+              <input type="password" required />
+              <span></span>
+              <label>Confirm password</label>
+            </div>
+
+            <input type="submit" name="submitButton" value="SUBMIT" />
+          </form>
+
+          <div className="signup_link">
+            <a href="#" className="signInMessage">Already have an account? Sign in here!</a>
+          </div>
         </div>
-
-        <div className="txt_field">
-          <input type="text" required />
-          <span></span>
-          <label>Last name</label>
-        </div>
-
-        <div className="txt_field">
-          <input type="text" required />
-          <span></span>
-          <label>Username</label>
-        </div>
-
-        <div className="txt_field">
-          <input type="email" required />
-          <span></span>
-          <label>Email</label>
-        </div>
-
-        <div className="txt_field">
-          <input type="email" required />
-          <span></span>
-          <label>Confirm email</label>
-        </div>
-
-        <div className="txt_field">
-          <input type="password" required />
-          <span></span>
-          <label>Password</label>
-        </div>
-
-        <div className="txt_field">
-          <input type="password" required />
-          <span></span>
-          <label>Confirm password</label>
-        </div>
-
-        <input type="submit" name="submitButton" value="SUBMIT" />
-      </form>
-      <div className="signup_link">
-        <a href="#" class="signInMessage">Already have an account? Sign in here!</a>
-      </div>
+        
     </div>
-  </div>
   )
 }
 
