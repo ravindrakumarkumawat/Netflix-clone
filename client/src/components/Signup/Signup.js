30146687b5eeeac6d68d7f2d82bb0d7183ca8ae9
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-07-24 23:16:48 +0530

    Css is updated

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,64 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
 
 function Signup (props) {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="Container">
         <div className="column">
-
-          <div className="header">        
-            <img src={ process.env.PUBLIC_URL + "images/jioflix.png" } title="logo" alt="logo" />
+          <div className="header">
             <h3>Ready to watch? Create an account</h3>
           </div>
 
           <form>
             <div className="txt_field">
-              <input type="text" required />
+              <input 
+                type="text" 
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required />
               <span></span>
               <label>First name</label>
             </div>
 
             <div className="txt_field">
-              <input type="text" required />
+              <input 
+                type="text" 
+                value={lastName} 
+                onChange={(e) => setLastName(e.target.value)}
+                required />
               <span></span>
               <label>Last name</label>
             </div>
 
             <div className="txt_field">
-              <input type="text" required />
+              <input 
+                type="text" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required />
               <span></span>
               <label>Username</label>
             </div>
 
             <div className="txt_field">
-              <input type="email" required />
+              <input 
+                type="email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              required />
               <span></span>
               <label>Email</label>
             </div>
 
             <div className="txt_field">
-              <input type="email" required />
-              <span></span>
-              <label>Confirm email</label>
-            </div>
-
-            <div className="txt_field">
-              <input type="password" required />
+              <input 
+                type="password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required />
               <span></span>
               <label>Password</label>
             </div>
 
-            <div className="txt_field">
-              <input type="password" required />
-              <span></span>
-              <label>Confirm password</label>
-            </div>
-
-            <input type="submit" value="SUBMIT" />
+            <input type="submit" value="Sign Up" />
           </form>
 
           <div className="signup_link">
-            <a href="#" className="signInMessage">Already have an account? Sign in here!</a>
+            <span>Already have an account?<a href="#" className="signInMessage"> Sign in here!</a></span>
           </div>
         </div>
 
