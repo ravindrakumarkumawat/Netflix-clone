e741e9ed88e2cc08c4d16fd1a625f74188305381
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-07-29 19:17:28 +0530

    playlist is added

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,14 @@ import Preview from './components/video/Preview'
 
 // const videoCategories = 'https://www.googleapis.com/youtube/v3/videoCategories?key=&part=snippet&h1=in&regionCode=IN'
 // const videos = 'https://www.googleapis.com/youtube/v3/videos?key=&part=snippet,contentDetails,statistics&regionCode=IN&chart=mostPopular&maxResults=10'                           
-// const channelsByID = 'https://www.googleapis.com/youtube/v3/channels?key=&part=snippet,contentDetails,statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw'
+// const channelsById = 'https://www.googleapis.com/youtube/v3/channels?key=&part=snippet,contentDetails,statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw'
 // const channelsByUsername = 'https://www.googleapis.com/youtube/v3/channels?key=&part=snippet,contentDetails,statistics&forUsername=GoogleDevelopers'
+// const channelSectionsById = 'https://www.googleapis.com/youtube/v3/channelSections?key=&part=snippet,contentDetails&channelId=UC_x5XG1OV2P6uZZ5FSM9Ttw'
+// const channelSectionsMine = 'https://www.googleapis.com/youtube/v3/channelSections?key=&part=snippet,contentDetails&mine=true'
+// const playlistsAll = 'https://www.googleapis.com/youtube/v3/playlists?key=&part=snippet,contentDetails&channelId=UC_x5XG1OV2P6uZZ5FSM9Ttw&maxResults=25'
+const playlistItems = ''
+const search = ''
+
 
 function App() {
   const [categories, setCategories] = useState([])
