69353d72e900036dff55c3146567e65ee93c2f6f
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-08-04 11:53:15 +0530

    Play, mute and unmute functionality is added

--- client/src/components/Preview/Preview.js
+++ client/src/components/Preview/Preview.js
@@ -4,17 +4,24 @@ import { faPlay, faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-ic
 import './Preview.css'
 
 function Preview ({url}) { 
-  const [mute, setMute] = useState(true)
+  const [mute, setMute] = useState(1)
+  const [autoPlay, setAutoPlay] = useState(1)
+  const toggleMute = () => {
+    setMute(Number(!mute))
+  }
+  const togglePlay = () => {
+    setAutoPlay(Number(!autoPlay))
+  }
   return (
     <div className='preview-container'>
-      <iframe src={`https://www.youtube.com/embed/${url}?vq=hd1080&controls=0&autoplay=0&loop=1&mute=1&cc_load_policy=0&playlist=${url}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe src={`https://www.youtube.com/embed/${url}?vq=hd1080&controls=0&autoplay=${autoPlay}&loop=1&mute=${mute}&cc_load_policy=0&playlist=${url}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       <div className='preview-overlay'>
         <div className='preview-main-details'>
           <h3>Video Title</h3>
-
+          <p>Video Subtitle</p>
           <div className='preview-overlay-buttons'>
-            <button><FontAwesomeIcon icon={faPlay}></FontAwesomeIcon> Play</button>
-            <button onClick={() => setMute(!mute)}> {
+            <button onClick={() => togglePlay()}><FontAwesomeIcon icon={faPlay}></FontAwesomeIcon> Play</button>
+            <button onClick={() => toggleMute()}> {
               (mute ? <FontAwesomeIcon icon={faVolumeMute}></FontAwesomeIcon>
               : <FontAwesomeIcon icon={faVolumeUp}></FontAwesomeIcon>
               )
