e77a55acd73d8325b6f7eb9df3cef4839248c301
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-08-07 11:17:02 +0530

    category wise video id is taken out

--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -4,66 +4,124 @@ import { videos, videoCategories } from '../../YoutubeApi'
 
 function Categories(props) {  
   const [categories, setCategories] = useState([])
-  const [video, setVideo] = useState([])
-  // const [selectCat, setSelectCat] = useState({})
-  const [nextPageToken, setNextPageToken] = useState('')
+  // const [catVideo, setCatVideo] = useState({})
+  // const [nextPageToken, setNextPageToken] = useState('')
 
   useEffect(() => {
     get_categories()
-    get_videos()
-    filter_videos()
+    // get_videos_by_pageToken()     
+    // get_videos()  
   }, [])
 
-  const get_categories = async () => {
-    await fetch(videoCategories).then(response => response.json()).then(res => {      
-      console.log('videoCategories API...')
-      const catItems = res.items.map((item) => {return {id: item.id, title:item.snippet.title, v_id: []}})
-      setCategories(catItems)
-    }).catch(error => console.log(error))
-  }
 
-  const get_videos = async () => {
-    let count = 0
-    while (count < 10) {
-      if(count === 0) {
-        await fetch(`${videos}&maxResults=50&`).then(response => response.json()).then(res => {          
-          console.log(res) 
-          setVideo(res.items)
-          setNextPageToken(res.nextPageToken)
-        }).catch(error => console.log(error))
-        count++
-      } else {
-        await fetch(`${videos}&maxResults=50&pageToken=${nextPageToken}`).then(response => response.json()).then(res => { 
-          console.log('Isme enter to hua'+ res.nextPageToken)
-          console.log(res.nextPageToken)
-          setNextPageToken(res.nextPageToken)
-        }).catch(error => console.log(error))
-        count++
+  const get_categories = async () => {
+
+    const cat_name = await fetch(videoCategories).then(response => response.json()).then(res => {      
+      console.log('videoCategories API...')
+      return res.items.map((item) => {return {id: item.id, title:item.snippet.title}})
+      // setCategories(catItems)
+    }).catch(error => console.log(error))
+
+    const cat_video = await fetch(`${videos}&maxResults=50`)
+                    .then(response => response.json())
+                    .then(res => {
+                      // setVideo(res.items)
+                      // setNextPageToken(res.nextPageToken)
+                      const vi = {}
+                      for(let item of res.items) {
+                        if(vi.hasOwnProperty(item.snippet.categoryId)) {
+                          vi[item.snippet.categoryId].push(item.id)
+                        }
+                        else {
+                           vi[item.snippet.categoryId] = []
+                        }
+                      }
+                      return vi
+                    })
+                    .catch(error => console.log(error))
+
+    const cat_video_list = []
+    for(let cat of cat_name) {
+      if(cat_video.hasOwnProperty(cat.id)) {
+        cat_video_list.push({
+          v_lists: cat_video[cat.id],
+          c_id: cat.id,
+          c_title: cat.title
+        })
       }
     }
-    console.log(video)
+    setCategories(cat_video_list)
+    
+  }
+
+  // const get_videos_by_pageToken = async () => {
+    
+  //   let count = 0
+  //   while (count < 10) {
+  //     if (nextPageToken) {
+  //       await fetch(`${videos}&maxResults=50&pageToken=${nextPageToken}`)
+  //                   .then(response => response.json())
+  //                   .then(res => {
+  //                     console.log(res.nextPageToken)
+  //                   })
+  //                   .catch(error => console.log(error))
+  //       count++
+  //     }
+  //   }
+  // }
+
+  const get_videos = async () => {
+    // await fetch(`${videos}&maxResults=50`)
+    //                 .then(response => response.json())
+    //                 .then(res => {
+    //                   // setVideo(res.items)
+    //                   // setNextPageToken(res.nextPageToken)
+    //                   const vi = {}
+    //                   for(let item of res.items) {
+    //                     if(vi.hasOwnProperty(item.snippet.categoryId)) {
+    //                       vi[item.snippet.categoryId].push(item.id)
+    //                     }
+    //                     else {
+    //                        vi[item.snippet.categoryId] = []
+    //                     }
+    //                   }
+    //                   setCatVideo(vi)  
+    //                 })
+    //                 .catch(error => console.log(error))
+
+    // let count = 0
+    // while (count < 10) {
+    //   if(count === 0) {
+    //     const res = await get_videos_by_pageToken(nextPageToken)
+    //     setVideo(res.items)        
+    //     count++
+    //   } else {
+    //     const res = await get_videos_by_pageToken(nextPageToken)
+    //     setVideo([...video, res.items])
+    //     count++
+    //   }
+    // }
   }
   
-  const filter_videos = () => {
+  const filter_videos = (vi) => { 
     
-
-    // const vi = {}
-    // for(let item of res.items) {
-    // if(vi.hasOwnProperty(item.snippet.categoryId)) {
-    //   vi[item.snippet.categoryId].push(item.id)
-    // }
-    // else {
-    //   vi[item.snippet.categoryId] = []
-    // }
-    // }
-    // setSelectCat(vi)
   }
 
   return (
-    <div className='preview-categories-container'>      
-      <h1>Something about categories.......</h1>
+    <div className='preview-categories-container'> 
       {
-        categories.map((cat) => <p key={cat.id}> {cat.id} {cat.title} </p>)
+        categories.map((cat) => 
+          <div>
+            <h2 key={cat.c_id}>{cat.c_title}</h2>
+            <ul>
+              {
+                cat.v_lists.map((list) => 
+                <li key={cat.c_id}>{list}</li>)
+              }
+            </ul>
+          </div>
+          
+        )
       }
     </div>
   )
