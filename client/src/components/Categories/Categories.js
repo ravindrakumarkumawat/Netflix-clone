21a6c91485a24d06a1a8e0bda66d35f52ff529a4
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-08-09 19:15:06 +0530

    Category videos are added to app component

--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -1,69 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Categories.css'
-import { videos, videoCategories } from '../../YoutubeApi'
+// import { videos, videoCategories } from '../../YoutubeApi'
 
-function Categories(props) {  
-  const [catVideo, setCatVideo] = useState([])
-
-  useEffect(() => {   
-    get_videos()  
-  }, [])
-
-  const get_videos = async () => {
-      const res = await fetch(`${videos}&maxResults=50`)
-                    .then(response => response.json())
-                    .then(res => res)
-                    .catch(error => console.log(error))
-
-      let cat_videos = res.items
-      let nextPageToken = res.nextPageToken
-      let count = 0
-      while(count < 3) {
-        const res = await get_videos_by_pageToken(nextPageToken)
-        nextPageToken = res.nextPageToken
-        cat_videos = [...cat_videos, ...res.items]
-        count++
-      }
-      
-      const categories = await get_categories()
-      
-      const vi = {}
-      for(let item of cat_videos) {
-        if(vi.hasOwnProperty(item.snippet.categoryId)) {
-          vi[item.snippet.categoryId].push({id: item.id, title:item.snippet.title, thumbnail: item.snippet.thumbnails.high})
-        }
-        else {
-          vi[item.snippet.categoryId] = []
-        }
-      }
-      
-      const cat_video_list = []
-      for(let cat of categories) {
-        if(vi.hasOwnProperty(cat.id)) {
-          cat_video_list.push({
-            v_lists: vi[cat.id],
-            c_id: cat.id,
-            c_title: cat.title
-          })
-        }
-      }
-      setCatVideo(cat_video_list)
-  }
-
-  const get_categories = async () => {
-
-    return await fetch(videoCategories).then(response => response.json()).then(res => {      
-      console.log('videoCategories API...')
-      return res.items.map((item) => {return {id: item.id, title:item.snippet.title}})
-    }).catch(error => console.log(error))  
-  }
-
-  const get_videos_by_pageToken = async (nextPageToken) => {
-    return  await fetch(`${videos}&maxResults=50&pageToken=${nextPageToken}`)
-                    .then(response => response.json())
-                    .then(res => res)
-                    .catch(error => console.log(error))
-  }
+function Categories({catVideo}) {  
+  
 
   return (
     <div className='preview-categories-container'> 
