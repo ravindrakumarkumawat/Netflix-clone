326e3b0155dd541a76502cc0307d090699815e8b
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-08-14 19:17:07 +0530

    Entity separate video is added

--- client/src/components/Entity/Entity.js
+++ client/src/components/Entity/Entity.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Entity.css'
 import Preview from '../Preview/Preview'
 import { useParams } from 'react-router-dom'
+import {video} from '../../YoutubeApi'
 
 function Entity() {
-  const {v_id} = useParams();
+  const {v_id} = useParams()
+  const [randomUrl, setRandomUrl] = useState('')
+  useEffect(() => {
+    fetch(`${video}${v_id}`).then((response)=> response.json()).then(res => {
+      setRandomUrl({id: res.items[0].id, title: res.items[0].snippet.title, thumbnails: res.items[0].snippet.thumbnails.high, channelId: res.items[0].snippet.channelId})
+    })
+  }, [])
   return (
-    <Preview randomUrl={v_id} />
+    <Preview randomUrl={randomUrl} />
   )
 } 
 
