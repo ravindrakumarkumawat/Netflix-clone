758be6ac382d439e18dd6b1de46059727e7ba958
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-08-25 22:00:47 +0530

    playlist is taken out is only one

--- client/src/YoutubeApi.js
+++ client/src/YoutubeApi.js
@@ -4,7 +4,7 @@ export const key = 'AIzaSyBP80ACJ3HRGpTiu24IgkayomPHfE5BGe8'
 export const videoCategories = `${baseUrl}/videoCategories?key=${key}&part=snippet&h1=us&regionCode=US`
 export const videos = `${baseUrl}/videos?key=${key}&part=snippet,contentDetails,statistics&h1=us&regionCode=US&chart=mostPopular`
 export const video = `${baseUrl}/videos?key=${key}&part=snippet,contentDetails,statistics&id=`
-export const playlists = `${baseUrl}/playlists?key=${key}&part=snippet,contentDetails&maxResults=5&channelId=`
+export const playlists = `${baseUrl}/playlists?key=${key}&part=snippet,contentDetails&maxResults=1&channelId=`
 export const playlistItems = `${baseUrl}/playlistItems?key=${key}&part=snippet,contentDetails&maxResults=10&playlistId=`
 export const relatedVideo = `${baseUrl}/search?key=${key}&part=snippet&maxResults=10&type=video&relatedToVideoId=`
 export const search = `${baseUrl}/search?key=${key}&part=snippet&maxResults=10&q=`
