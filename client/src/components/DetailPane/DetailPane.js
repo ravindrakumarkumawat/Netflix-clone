1d86c503a2f5380f9ec7b08773f9c18cd07f9eff
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-08-31 16:53:10 +0530

    video is added on the detail panel

--- client/src/components/DetailPane/DetailPane.js
+++ client/src/components/DetailPane/DetailPane.js
@@ -21,23 +21,28 @@ function DetailPane ({ category, setActive, id, title, channelTitle }) {
   // }
   return (
     category && (        
-    <div className='detailPane-container'>
-      <div className='pane-wrapper'>
-        <h3 style={{
-          fontSize: `${tab==='Overview' ? '70px' : '50px'}`
-        }}>{channelTitle}</h3>
-        {(() => {
-          switch (tab) {
-            case 'Playlists':
-              return <Playlists v_id={id}/>
-            case 'More Like This':
-              return <RelatedVideo related_vid={id} />
-            default:
-              return <Overview title={title} id={id} />
-          }
-        })()}
-        <FontAwesomeIcon icon={faTimes} className='Icon' onClick={setActive}></FontAwesomeIcon>
-        <DetailPaneNav tabs={TABS} tab={tab} setTab={setTab}/>        
+    <div className='detailPane-container'><div className='pane-wrapper'>
+        { tab=== 'Overview' && (
+            <iframe title={category} src={`https://www.youtube.com/embed/${id}?vq=hd1080&controls=0&autoplay=1&loop=1&mute=1&cc_load_policy=0&playlist=${id}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
+          )
+        }
+        <div className='detailPane-overlay'>
+          <h3 style={{
+            fontSize: `${tab==='Overview' ? '70px' : '50px'}`
+          }}>{channelTitle}</h3>
+          {(() => {
+            switch (tab) {
+              case 'Playlists':
+                return <Playlists v_id={id}/>
+              case 'More Like This':
+                return <RelatedVideo related_vid={id} />
+              default:
+                return <Overview title={title} id={id} />
+            }
+          })()}
+          <FontAwesomeIcon icon={faTimes} className='Icon' onClick={setActive}></FontAwesomeIcon>
+          <DetailPaneNav tabs={TABS} tab={tab} setTab={setTab}/>  
+        </div>      
       </div>
     </div>
     )
