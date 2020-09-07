25d0fd5bff98413b324a4383a2f0d7fd5a36ac88
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-07 20:16:08 +0530

    category is added inside category

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './App.css'
-import Preview from './components/Preview/Preview'
 import Navbar from './components/navbar/Navbar'
 import Categories from './components/Categories/Categories'
 import Watch from './components/Watch/Watch'
 import Search from './components/Search/Search'
-import { videos, videoCategories } from './YoutubeApi'
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,72 +14,7 @@ import {
 // import Register from './components/Register/Register'
 
 function App() {
-  const [catVideo, setCatVideo] = useState([])
-  const [randomUrl, setRandomUrl] = useState('')
   
-  useEffect(() => {   
-    get_videos()  
-  }, [])
-
-  const get_videos = async () => {
-      const res = await fetch(`${videos}&maxResults=50`)
-                    .then(response => response.json())
-                    .then(res => res)
-                    .catch(error => console.log(error))
-
-      let cat_videos = res.items
-      // let nextPageToken = res.nextPageToken
-      // let count = 0
-      // while(count < 3) {
-      //   const res = await get_videos_by_pageToken(nextPageToken)
-      //   nextPageToken = res.nextPageToken
-      //   cat_videos = [...cat_videos, ...res.items]
-      //   count++ 
-      // }
-      
-      const categories = await get_categories()
-      
-      const vi = {}
-      for(let item of cat_videos) {
-        if(vi.hasOwnProperty(item.snippet.categoryId)) {
-          vi[item.snippet.categoryId].push({id: item.id, title:item.snippet.title, channelTitle:item.snippet.channelTitle, thumbnail: item.snippet.thumbnails.medium, channelId: item.snippet.channelId})
-        }
-        else {
-          vi[item.snippet.categoryId] = []
-        }
-      }
-      
-      const cat_video_list = []
-      for(let cat of categories) {
-        if(vi.hasOwnProperty(cat.id)) {
-          if(vi[cat.id].length > 0) {            
-            cat_video_list.push({
-              v_lists: vi[cat.id],
-              c_id: cat.id,
-              c_title: cat.title
-            })
-          }
-        }
-      }
-      const url = cat_video_list[Math.floor(Math.random() * cat_video_list.length)].v_lists
-      setRandomUrl(url[Math.floor(Math.random() * url.length)])
-      setCatVideo(cat_video_list)
-  }
-
-  const get_categories = () => {
-
-    return fetch(videoCategories).then(response => response.json()).then(res => {
-      return res.items.map((item) => {return {id: item.id, title:item.snippet.title}})
-    }).catch(error => console.log(error))  
-  }
-
-  const get_videos_by_pageToken = (nextPageToken) => {
-    return fetch(`${videos}&maxResults=50&pageToken=${nextPageToken}`)
-                    .then(response => response.json())
-                    .then(res => res)
-                    .catch(error => console.log(error))
-  }
-
   return (
     <Router>
       <div className="App"> 
@@ -96,9 +29,8 @@ function App() {
           */           
          } 
           <Route path="/browse" exact>          
-            <Navbar/>         
-            <Preview randomUrl={randomUrl} />
-            <Categories catVideo={catVideo} />
+            <Navbar/>
+            <Categories />
           </Route>
           {/*<Route path="/tv-shows" exact>  
             <h2 className='ErrorMessage'>This is tv-shows</h2> 
--- client/src/components/Categories/Categories.js
+++ client/src/components/Categories/Categories.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Categories.css'
+import Preview from '../Preview/Preview'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import DetailPane from '../DetailPane/DetailPane'
+import { videos, videoCategories } from '../../YoutubeApi'
 
 const initialRow = {
   category: '',
@@ -12,7 +14,7 @@ const initialRow = {
   channelTitle: ''
 }
 
-function Categories({ catVideo }) { 
+function Categories() { 
   const [activeRow, setActiveRow] = useState(initialRow)
   const {
     category,
@@ -23,7 +25,76 @@ function Categories({ catVideo }) {
   const setActive = (activeRow) => {
     activeRow.category ? setActiveRow(activeRow) : setActiveRow(initialRow)
   }
+
+  const [catVideo, setCatVideo] = useState([])
+  const [randomUrl, setRandomUrl] = useState('')
+  
+  useEffect(() => {   
+    get_videos()  
+  }, [])
+
+  const get_videos = async () => {
+      const res = await fetch(`${videos}&maxResults=50`)
+                    .then(response => response.json())
+                    .then(res => res)
+                    .catch(error => console.log(error))
+
+      let cat_videos = res.items
+      // let nextPageToken = res.nextPageToken
+      // let count = 0
+      // while(count < 3) {
+      //   const res = await get_videos_by_pageToken(nextPageToken)
+      //   nextPageToken = res.nextPageToken
+      //   cat_videos = [...cat_videos, ...res.items]
+      //   count++ 
+      // }
+      
+      const categories = await get_categories()
+      
+      const vi = {}
+      for(let item of cat_videos) {
+        if(vi.hasOwnProperty(item.snippet.categoryId)) {
+          vi[item.snippet.categoryId].push({id: item.id, title:item.snippet.title, channelTitle:item.snippet.channelTitle, thumbnail: item.snippet.thumbnails.medium, channelId: item.snippet.channelId})
+        }
+        else {
+          vi[item.snippet.categoryId] = []
+        }
+      }
+      
+      const cat_video_list = []
+      for(let cat of categories) {
+        if(vi.hasOwnProperty(cat.id)) {
+          if(vi[cat.id].length > 0) {            
+            cat_video_list.push({
+              v_lists: vi[cat.id],
+              c_id: cat.id,
+              c_title: cat.title
+            })
+          }
+        }
+      }
+      const url = cat_video_list[Math.floor(Math.random() * cat_video_list.length)].v_lists
+      setRandomUrl(url[Math.floor(Math.random() * url.length)])
+      setCatVideo(cat_video_list)
+  }
+
+  const get_categories = () => {
+
+    return fetch(videoCategories).then(response => response.json()).then(res => {
+      return res.items.map((item) => {return {id: item.id, title:item.snippet.title}})
+    }).catch(error => console.log(error))  
+  }
+
+  const get_videos_by_pageToken = (nextPageToken) => {
+    return fetch(`${videos}&maxResults=50&pageToken=${nextPageToken}`)
+                    .then(response => response.json())
+                    .then(res => res)
+                    .catch(error => console.log(error))
+  }
+  
   return (
+    <>
+    <Preview randomUrl={randomUrl} />
     <div className='preview-categories-container'> 
       {
         catVideo.map((cat, index) =>  
@@ -39,6 +110,7 @@ function Categories({ catVideo }) {
         )
       }
     </div>
+    </>
   )
 }
 
