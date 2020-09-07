b1b7ad4c9ac6454ec05b616a231fcfe48574afe3
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-07 12:45:14 +0530

    Email Adderess is added and login state is added

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,30 +7,30 @@ function Login (props) {
 
   return login ? (
     <Redirect to='/browse' />
-  ) : (
+    ) : (
     <div className="Container">
-    <div className="column">
-      <div className="header"> 
-        <h3>Let The Journey Begin !</h3>
-      </div>
-      <form>
-        <div className="txt_field">          
-          <input type="text" required />
-          <span></span>
-          <label>Username</label>
+      <div className="column">
+        <div className="header"> 
+          <h3>Let The Journey Begin !</h3>
         </div>
-        <div className="txt_field">          
-          <input type="password" required />
-          <span></span>
-          <label>Password</label>
+        <form>
+          <div className="txt_field">          
+            <input type="email" name='email' required />
+            <span></span>
+            <label>Email Address</label>
+          </div>
+          <div className="txt_field">          
+            <input type="password" name='password' required />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+        <div className="signin_link">
+          <Link to="/register"><span className="signInMessage">New to Netflix? Sign up now.</span></Link>
         </div>
-        <input type="submit" value="Login" />
-      </form>
-      <div className="signin_link">
-        <Link to="/register"><span className="signInMessage">New to Netflix? Sign up now.</span></Link>
+        
       </div>
-      
-    </div>
   </div>
   )
 }
