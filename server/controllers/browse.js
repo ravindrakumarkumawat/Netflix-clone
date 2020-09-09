633bb62721acbf4e142b475d34ac1b34b8dc7853
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-09 22:28:01 +0530

    browse data changed

--- server/controllers/browse.js
+++ server/controllers/browse.js
@@ -3,8 +3,8 @@ const { videos } = require('../YoutubeApi')
 
 const get_videos = async (req, res) => {
   try {
-    // const response = await axios.get(`${videos}&maxResults=50`)
-    res.send('response')
+    const response = await axios.get(`${videos}&maxResults=50`)
+    res.json(response.data)
   } catch (err) {
     console.log(err.message)
   }
