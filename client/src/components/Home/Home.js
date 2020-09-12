0d9389c54e84a28c3c67b81116b23c96439c3dc4
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-12 10:35:31 +0530

    Home page style is completed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Home.css'
 
 function Home() {
   return (
-    <ul>            
-      <li><Link to='/login'>Login</Link></li>
-      <li><Link to='/register'>Register</Link></li>
-    </ul>
+    <header>
+    <nav className='home-container'>
+      <Link to='/' className='logo'>Vivid</Link>   
+      <Link to='/register' className='signup'>Sign up</Link>    
+      <Link to='/login' className='signin'>Sign in</Link>
+    </nav>
+    <section className="pitch">
+      <h1 className="pitch__title">Unlimited movies, TV shows and more.</h1>
+      <h2 className="pitch__subtitle">Watch anywhere. Cancel anytime.</h2>
+    </section>
+    </header>
   )
 }
 
