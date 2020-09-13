a664943e441d41a2303d113fc3dba932aa82266b
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-13 20:23:50 +0530

    upload video structure is created

--- client/src/components/Studio/Studio.js
+++ client/src/components/Studio/Studio.js
@@ -1,46 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Studio.css'
-import StudioPaneNav from './StudioPaneNav/StudioPaneNav'
-import Overview from '../Overview/Overview'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
-import Playlists from '../Playlists/Playlists'
-import RelatedVideo from '../RelatedVideo/RelatedVideo'
-
-const TABS = ['Overview', 'Playlists', 'More Like This']
+import UploadVideo from './UploadVideo/UploadVideo'
 
 function Studio () {
-  const [tab, setTab] = useState()
-
-  useEffect(() => {
-    setTab(TABS[0])
-  }, [])
-
-  // const style = {
-  //   top: top + 'px'
-  // }
   return (        
-    <div className='detailPane-container'><div className='pane-wrapper'>
-        {/* tab=== 'Overview' && (
-            <iframe title={category} src={`https://www.youtube.com/embed/${id}?vq=hd1080&controls=0&autoplay=1&loop=1&mute=1&cc_load_policy=0&playlist=${id}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
-          )
-        */}
-        <div className='detailPane-overlay'>
-          {/*<h3 style={{
-            fontSize: `${tab==='Overview' ? '70px' : '50px'}`
-          }}>{channelTitle}</h3>*/}
-          {(() => {
-            switch (tab) {
-              case 'Playlists':
-                return <Playlists />
-              case 'More Like This':
-                return <RelatedVideo />
-              default:
-                return <Overview />
-            }
-          })()}
-          <FontAwesomeIcon icon={faTimes} className='Icon'></FontAwesomeIcon>
-          <StudioPaneNav tabs={TABS} tab={tab} setTab={setTab}/>  
+    <div className='studioPane-container'>
+      <div className='pane-wrapper'>
+        <div className='studioPane-overlay'>
+          <UploadVideo /> 
         </div>      
       </div>
     </div>
