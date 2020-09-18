60c73a06ca9dafb0e0c91d7fdc55838ef9b23030
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-18 10:22:15 +0530

    api is added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Categories from './components/Categories/Categories'
@@ -11,11 +11,73 @@ import {
   Route, Link
 } from 'react-router-dom'
 
+
+import {CLIENT_ID, API_KEY} from './OAuth.config'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Home from './components/Home/Home'
 
+const clientId = CLIENT_ID
+const apiKey = API_KEY
+
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(null)
+ 
+  useEffect(() => {
+    console.log('Loading')
+
+    insertGapiScript();
+  }, [])
+
+  const insertGapiScript = () => {
+    const script = document.createElement('script')
+    script.src = 'https://apis.google.com/js/platform.js'
+    script.onload = () => {
+      initializeGoogleSignIn()
+    }
+    document.body.appendChild(script)
+  }
+
+  const initializeGoogleSignIn = () => {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2.init({
+        client_id: clientId
+      }).then(()=> {
+        const authInstance = window.gapi.auth2.getAuthInstance()
+        const isSignedIn = authInstance.isSignedIn.get()
+        setIsSignedIn(isSignedIn)
+
+        authInstance.isSignedIn.listen(isSignedIn => {
+          setIsSignedIn(isSignedIn)
+        })
+      })
+      console.log('Api inited')
+
+      window.gapi.load('signin2', () => {
+        const params = {
+          scope: 'profile email',
+          width: 240,
+          height: 50,
+          longtitle: true,
+          theme: 'dark',
+          onsuccess: (res) => {
+            console.log('User has finished signing in!', res)
+          },
+          onfailure: (res) => {
+            console.log('Login failed: res:', res)
+          }
+        }
+        window.gapi.signin2.render('loginButton', params)
+      })
+
+      // const authInstance = window.gapi.auth2.getAuthInstance()
+      // const user = authInstance.currentUser.get()
+      // const profile = user.getBasicProfile()
+      // const email = profile.getEmail()
+      // const imageUrl = profile.getImageUrl()
+      //onclick={authInstance.signOut}
+    })
+  }
   
   return (
     <Router>
@@ -37,31 +99,20 @@ function App() {
             <Navbar/>
             <Categories />
           </Route>
-          {/*<Route path="/tv-shows" exact>  
-            <h2 className='ErrorMessage'>This is tv-shows</h2> 
-          </Route>
-          <Route path="/movies" exact>
-            <h2 className='ErrorMessage'>This is movies page</h2>
-          </Route>
-          <Route path="/recently-added" exact>
-            <h2 className='ErrorMessage'>This is recently page</h2>
-          </Route>
-          <Route path="/details/:v_id" exact>                      
-            <Entity />
-          </Route>
-          <Route path="/details" exact>                      
-            <h2 className='ErrorMessage'>No id is passed in url</h2>
-          </Route>*/}
+
           <Route path="/watch/:v_id" exact>                      
             <Watch />
           </Route>
+
           <Route path="/watch" exact>                      
             <h2 className='ErrorMessage'>No id is passed in url</h2>
           </Route>
+
           <Route path="/search" exact> 
             <Navbar/>                      
             <Search />
           </Route>
+          
           <Route path='/' exact>
             <Home />
           </Route>
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,83 +1,9 @@
-import React, {useState, useEffect} from 'react'
-import {CLIENT_ID, API_KEY} from '../../OAuth.config'
-import {Link, Redirect} from 'react-router-dom'
-
-const clientId = CLIENT_ID
-const apiKey = API_KEY
-  
+import React from 'react'
+import {Link, Redirect} from 'react-router-dom'  
 
 function Register() {
-  const [isSignedIn, setIsSignedIn] = useState(null)
- 
-  useEffect(() => {
-    console.log('Loading')
 
-    insertGapiScript();
-  }, [])
-
-  const insertGapiScript = () => {
-    const script = document.createElement('script')
-    script.src = 'https://apis.google.com/js/platform.js'
-    script.onload = () => {
-      initializeGoogleSignIn()
-    }
-    document.body.appendChild(script)
-  }
-
-  const initializeGoogleSignIn = () => {
-    window.gapi.load('auth2', () => {
-      window.gapi.auth2.init({
-        client_id: clientId
-      }).then(()=> {
-        const authInstance = window.gapi.auth2.getAuthInstance()
-        const isSignedIn = authInstance.isSignedIn.get()
-        setIsSignedIn(isSignedIn)
-
-        authInstance.isSignedIn.listen(isSignedIn => {
-          setIsSignedIn(isSignedIn)
-        })
-      })
-      console.log('Api inited')
-
-      window.gapi.load('signin2', () => {
-        const params = {
-          scope: 'profile email',
-          width: 240,
-          height: 50,
-          longtitle: true,
-          theme: 'dark',
-          onsuccess: (res) => {
-            console.log('User has finished signing in!', res)
-          },
-          onfailure: (res) => {
-            console.log('Login failed: res:', res)
-          }
-        }
-        window.gapi.signin2.render('loginButton', params)
-      })
-
-      // const authInstance = window.gapi.auth2.getAuthInstance()
-      // const user = authInstance.currentUser.get()
-      // const profile = user.getBasicProfile()
-      // const email = profile.getEmail()
-      // const imageUrl = profile.getImageUrl()
-      //onclick={authInstance.signOut}
-    })
-  }
-
-  
-
-  // const onSuccess = (res) => {
-  //   console.log('Login Success: currentUser:', res)   
-  // }
-
-  // const onFailure = (res) => {
-  //   console.log('Login failed: res:', res)
-  // }
-
-  return isSignedIn ? (
-    <Redirect to='/browse' />
-  ):(
+  return (
     <header>
      {/* <nav className='home-container'>
         <Link to='/' className='logo'>Vivid</Link>     
