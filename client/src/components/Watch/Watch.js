b3bb381399d05e78b3fa298dd09eced76cda02f5
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-23 19:51:10 +0530

    back browse button is added in watch

--- client/src/components/Watch/Watch.js
+++ client/src/components/Watch/Watch.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useParams, Redirect } from 'react-router-dom'
+import { useParams, Redirect, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons'
 import './Watch.css'
 
 function Watch({isSignedIn, idToken}) {
@@ -8,6 +10,11 @@ function Watch({isSignedIn, idToken}) {
     <Redirect to='/register' />
   ):(
     <div className='watch-container'>
+      <div className='back-browser-link'>        
+        <Link to='/browse'>
+          <FontAwesomeIcon icon={faLongArrowAltLeft}></FontAwesomeIcon> Back to browser
+        </Link>
+      </div>  
       <iframe title='something' src={`https://www.youtube.com/embed/${v_id}?vq=hd1080&controls=1&autoplay=1&loop=1&mute=0&cc_load_policy=0&playlist=${v_id}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
     </div>
   )
