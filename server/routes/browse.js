873c0db142f3a6377fca48a7151d2e06c11f40f4
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-09-24 17:48:21 +0530

    server is removed future reference pickup from here

--- server/routes/browse.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const browse = require('../controllers/browse')
-
-router.get('/', browse.get_videos)
-
-module.exports = router
