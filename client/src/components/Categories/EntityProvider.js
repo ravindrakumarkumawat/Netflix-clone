a2bb9991dc8a30154abb4a5ec331e472a4601f78
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-21 19:37:10 +0530

    separate the provider

---
+++ client/src/components/Categories/EntityProvider.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfoCircle, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+const EntityProvider = ({ entity, setActive, category }) => {
+  const [hovered, setHovered] = useState(false) 
+
+  const handleHover = (e, id) => {
+    e.type === 'mouseenter'
+      ? setHovered(id)
+      : setHovered(false)
+  }
+
+  const getActive =  (e, id, title, channelTitle) => {
+    setActive({ category, id, title, channelTitle })
+  }
+
+  return (    
+    <div className='entity__inner'>
+    {
+      entity.map((list)=>
+        <div 
+          key={list.id} 
+          className='item'
+          onMouseEnter={(e)=> handleHover(e, list.id)}
+          onMouseLeave={handleHover}>
+
+        { list.id === hovered && (
+            <div className='item__details'>
+              <Link to={`/watch/${list.id}`}><FontAwesomeIcon icon={faPlayCircle} className='Icon'></FontAwesomeIcon></Link>
+              <FontAwesomeIcon icon={faInfoCircle} onClick={(e) => getActive(e, list.id, list.title, list.channelTitle)} className='Icon'></FontAwesomeIcon>            
+            </div>
+          )
+        } 
+        <div className='item__media'> 
+          { list.id === hovered
+            ? <iframe title={list.title} src={`https://www.youtube.com/embed/${list.id}?vq=hd1080&controls=0&autoplay=1&loop=1&mute=0&cc_load_policy=0&playlist=${list.id}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
+            : <img src={list.thumbnail.url} alt={list.title} className='item__img' />
+          }        
+        </div> 
+      </div>)
+    }
+    </div>
+  )
+} 
+
+export default EntityProvider
--- client/src/components/Categories/index.js
+++ client/src/components/Categories/index.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './index.css'
 import Preview from '../Preview'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfoCircle, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
 import DetailPane from '../DetailPane'
 import { videos, videoCategories } from '../../YoutubeApi'
 import {Redirect} from 'react-router-dom'
 import Navbar from '../Navbar'
+import EntityProvider from './EntityProvider'
 
 const initialRow = {
   category: '',
@@ -126,47 +124,5 @@ function Categories({accessToken, idToken, isSignedIn, handleSignoutClick, user}
     </>
   )
 }
-
-function EntityProvider({ entity, setActive, category }) {
-  const [hovered, setHovered] = useState(false) 
-
-  const handleHover = (e, id) => {
-    e.type === 'mouseenter'
-      ? setHovered(id)
-      : setHovered(false)
-  }
-
-  const getActive =  (e, id, title, channelTitle) => {
-    setActive({ category, id, title, channelTitle })
-  }
-
-  return (    
-    <div className='entity__inner'>
-    {
-      entity.map((list, index)=>
-        <div 
-          key={list.id} 
-          className='item'
-          onMouseEnter={(e)=> handleHover(e, list.id)}
-          onMouseLeave={handleHover}>
-        { list.id === hovered && (
-          <div className='item__details'>
-            <Link to={`/watch/${list.id}`}><FontAwesomeIcon icon={faPlayCircle} className='Icon'></FontAwesomeIcon></Link>
-            <FontAwesomeIcon icon={faInfoCircle} onClick={(e) => getActive(e, list.id, list.title, list.channelTitle)} className='Icon'></FontAwesomeIcon>            
-          </div>
-        )
-        } 
-        <div className='item__media'> 
-          { list.id === hovered
-            ? <iframe title={list.title} src={`https://www.youtube.com/embed/${list.id}?vq=hd1080&controls=0&autoplay=1&loop=1&mute=0&cc_load_policy=0&playlist=${list.id}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
-            : <img src={list.thumbnail.url} alt={list.title} className='item__img' />
-          }        
-        </div> 
-        </div>  
-      )
-    }
-    </div>
-  )
-} 
 
 export default Categories
