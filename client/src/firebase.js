6379f8b74de00ae3177dcadf368336c8c75bff6b
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-22 18:44:54 +0530

    file name changed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import firebaseConfig from './firebase.config'
+import firebaseConfig from './firebase1.config'
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
