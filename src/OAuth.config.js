3a14e15e47559dda0d0c78500f4bbbf243e7b71e
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-24 12:15:54 +0530

    config files

---
+++ src/OAuth.config.js
@@ -0,0 +1,4 @@
+export const CLIENT_ID='91971589582-a9ss150kmto7a95s28grunb40dacrn7b.apps.googleusercontent.com'
+export const CLIENT_SECRET='s68SGNDL4OKHifU2wNH36k1w'
+export const API_KEY='AIzaSyAXKXZ-sM_1VnWAiZVE9xa0pAURMdvAxfs'
+export const SCOPE='https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner-channel-audit'
---
+++ src/firebase1.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAk8UJfOX2Gvv9UjNF2AmijZE1jiJZ5fPo",
+  authDomain: "zephyr-videos.firebaseapp.com",
+  databaseURL: "https://zephyr-videos.firebaseio.com",
+  projectId: "zephyr-videos",
+  storageBucket: "zephyr-videos.appspot.com",
+  messagingSenderId: "8197159734",
+  appId: "1:8197159734:web:4437da1853fd9c37915197",
+  measurementId: "G-GXZJXFES06"
+}
+
+export default firebaseConfig
